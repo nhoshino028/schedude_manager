b55472c1da3b0d370e7e2d49b805e126f628bbf2
nhoshino028 <n.hoshino028@gmail.com>
2026-04-20 17:49:03 +0900

    API作成のための設定ファイル作成

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
+#pythonパッケージの初期化を行うときに利用される
+
 from flask import Flask
 from pydantic_settings import BaseSettings
 
@@ -7,6 +9,12 @@ class Settings(BaseSettings):
 settings = Settings()
 
 app = Flask(__name__)
+
+# blueprintの登録
+# 機能一つ一つをBluepointとする
+# 機能ごとに作成したbluepointを__init__.pyに登録してアプリに組み込む）
+
+# app.register_bluepoint() メソッドを使う
 
 @app.route('/')
 def home():
