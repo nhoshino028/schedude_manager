a9575699bf7f76f9ec77ee8aaff6b76404106af5
nhoshino028 <n.hoshino028@gmail.com>
2026-04-22 17:44:34 +0900

    ルーティング設定の追加

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,23 @@
 #pythonパッケージの初期化を行うときに利用される
 
-from flask import Flask, jsonify, request
-from database import pool
+import json
+from flask import Flask, jsonify
+import psycopg
 from psycopg.rows import dict_row
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
 
 class Settings(BaseSettings):
     app_name: str = "My Flask App"
-
+    database_url: str
+    DEBUG: bool = False
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    
+#.envファイルを見に行く
 settings = Settings()
 
+#flaskアプリを初期化
 app = Flask(__name__)
 
 # blueprintの登録
@@ -19,11 +28,82 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Welcome to {settings.app_name}!"
+    return f"Welcome to {Settings.app_name}!"
 
-
+#teamsテーブルの取得
 @app.route('/teams', methods=['GET'])
 def get_teams():
+    with psycopg.connect(settings.database_url) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute("SELECT * FROM teams")
+    teams = cur.fetchall()
+
+    return jsonify(teams)
+
+
+#usersテーブルの取得  クエリの部分とネストで返す部分がよく分かんないのでとりあえず基本形として作成
+@app.route('/users', methods=['GET'])
+def get_users():
+    with psycopg.connect(settings.database_url) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+
+    return jsonify(users)
+    
+
+#work-status-typesテーブルの取得
+@app.route('/work-status-types', methods=['GET'])
+def get_types():
+    with psycopg.connect(settings.database_url) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute("SELECT * FROM work-status-types")
+    work_status_types = cur.fetchall()
+
+    return jsonify(work_status_types)
+
+
+#schedulesテーブルの取得、データの登録
+@app.route('/schedules', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def schedules():
+    #取得
+    if request.method == 'GET':
+        with psycopg.connect(settings.database_url) as conn:
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute("SELECT * FROM schedules")
+        return jsonify(cur.fetchall())
+
+    #登録
+    else request.method == 'POST':
+        with psycopg.connect(settings.database_url) as conn:
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute(
+                    "INSERT INTO schedules (id, user_id, target_date, status_type_id, start_time, end_time, comment, created_at, updated_at) "
+                    "VALUES"
+                    "('5', '006', '2026-04-16', '06', null, null, '体調不良', '2026-04-16 08:30:00', '2026-04-16 08:30:00');"
+                )
+    
+#schedulesテーブルの更新、削除
+@app.route('/schedules/{id}', methods=['PUT', 'DELETE'])
+    #更新(ちょっと悩む)
+def edit_schedules():
+    if request.method == 'PUT':
+        with psycopg.connect(settings.database_url) as conn:
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute(
+                    "UPDATE monthly_schedule_summary SET absence_days = absence_days +1"
+                    "WHERE user_id = 5;"
+                )
+    
+    #削除
+    else request.method == 'DELETE':
+         with psycopg.connect(settings.database_url) as conn:
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute(
+                    "DELETE FROM schedules"
+                    "WHERE user_id = '2';"
+                )
+
 
 
 
--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    DATABASE_URL: str
-    DEBUG: bool = False
-
-model_config = SettingsConfigDict(env_file=".env")
-
-settings = Settings()
